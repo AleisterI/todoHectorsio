7081adbe20d4ef1fdae6746642ed6e0c5d674597
Aleister <88504681+AleisterI@users.noreply.github.com>
2023-05-03 12:59:49 -0300

    se agregaron cambios

--- controllers/myController.js
+++ controllers/myController.js
@@ -1,5 +1,22 @@
 const OneModel = require("../models/myModel");
 
-exports.inicio = (req, res) => {
-    res.status(200).render("index");
-}
+//exports.inicio = (req, res) => {
+//    res.status(200).render("index");
+//}
+
+const mongoose = require('mongoose');
+const path = require("path")
+const morgan = require("morgan")
+const express = require('express')
+const app = express()
+
+const autoesquema = new mongoose.Schema({
+    name: String
+});
+
+const automodel = mongoose.model("auto", autoesquema);
+const auto = new automodel({name: "fifi"});
+await auto.save();
+console.log(auto.name);
+
+app.listen(3000)
--- server.js
+++ server.js
@@ -13,5 +13,5 @@ app.listen(3000)
 const uri = 'mongodb+srv://admin:admin@sisis.lnzidxa.mongodb.net/test';
 
 mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true })
-    .then(() => console.log('Conexión establecida correctamente a la base de datos'))
+    .then(() => console.log('Conexión establecida correctamente a la base de datos', ))
     .catch(err => console.error('Error al conectarse a la base de datos', err));
